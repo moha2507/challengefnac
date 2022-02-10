05363815ced848eaf0212869a31bb67a10501a23
moha2507 <mohamedouali153@gmail.com>
2022-02-10 13:24:40 +0100

    challenge film finit

--- bibliographi.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $book_stevenson["l'île au tresort"]= '1883';
-    $book_stevenson["les nouvelles mille et un nuit"]= '1878';
-    $book_stevenson["prince othon"]= '1885';
-    asort($book_stevenson);
-
-    foreach ($book_stevenson as $books => $years)
-    echo ($books . ' ' . $years);
-    
